aa3b6058a5b44e7ca6b3e13a7bfa6245778af0ff
HanHtikeSoe1 <40667702@live.napier.ac.uk>
2024-01-25 22:57:58 +0630

    Employee information database query collection in app and main

---
+++ src/main/java/com/napier/sem/Main.java
@@ -0,0 +1,17 @@
+package com.napier.sem;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+}
